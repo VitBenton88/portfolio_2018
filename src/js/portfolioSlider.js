da50a50fd4b996d717019ec731ac9411bafb5314
Vit Benton <viteksk8alot@gmail.com>
2018-05-20 17:48:26 -0400

    Added skills graphs, added slick slider lib, started portfolio slider

---
+++ src/js/portfolioSlider.js
@@ -0,0 +1,28 @@
+const sliderSettings = {
+    arrows: false,
+    cssEase: 'linear',
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    swipeToSlide: true
+}
+
+const leftArrow = $('.fa-chevron-circle-left');
+const rightArrow = $('.fa-chevron-circle-right');
+const portfolioSlider = $('.portfolio-slider');
+
+$(document).ready(() => {
+    //init slider
+    portfolioSlider.slick(sliderSettings);
+
+    //leveraging slick methods
+    //custom next slide event
+    rightArrow.click(() => {
+        portfolioSlider.slick('slickNext');
+    });
+    //custom prev slide event
+    leftArrow.click(() => {
+        portfolioSlider.slick('slickPrev');
+    });
+});
