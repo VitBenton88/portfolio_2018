cb19c5c1f9e49a9b267ac6e2c418c758cb4db8d2
Vit Benton <viteksk8alot@gmail.com>
2018-07-29 17:44:24 -0400

    Added browser warning modal for outdated browsers (IE)

---
+++ src/js/browserWarning.js
@@ -0,0 +1,6 @@
+const browserWarningModal = $('#browser-warning');
+const browserWarningCloseIcon = $('#browser-warning .fa-times');
+
+browserWarningCloseIcon.click(()=> {
+    browserWarningModal.hide();
+})
