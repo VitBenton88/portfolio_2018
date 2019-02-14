ece78cfc6faf54ac843a8d2d2d9f9f522d901694
Vit Benton <viteksk8alot@gmail.com>
2019-02-13 21:43:47 -0500

    Added overlay to add nav item

--- source/admin/scripts/admin.js
+++ source/admin/scripts/admin.js
@@ -1,4 +1,21 @@
+/**
+* INIT
+**/
+
+
 //initialize Froala editor
 $(function() { 
     $('#aboutEditor').froalaEditor();
-}); 
+});
+
+/**
+* CLICK
+**/
+
+$('.add-item-overlay').click(function() {
+    if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+    } else {
+        $(this).addClass('active');
+    }
+})
